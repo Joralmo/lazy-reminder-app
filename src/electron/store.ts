0c7c6a577e468b4fbb0e3194baca6935c44565dd
Joralmo <JoralmoPro@Gmail.com>
2021-04-18 13:12:05 -0500

    refactor Storage class :recycle:

--- src/electron/store.ts
+++ src/electron/store.ts
@@ -1,20 +1,35 @@
-import Store from "electron-store";
+import Store, { Schema } from "electron-store";
 
-export default class LocalStorage {
-  #store: Store;
+const schema: Schema<Record<string, unknown>> = {
+  showBreaksIn: {
+    type: "number",
+    maximum: 2,
+    minimum: 1,
+    default: 2,
+  },
+  intervalForBreaks: {
+    type: "number",
+    maximum: 60,
+    minimum: 5,
+    default: 15,
+  },
+};
+
+export class LocalStorage {
+  private store: Store;
   constructor() {
-    this.#store = new Store();
+    this.store = new Store({ schema });
   }
 
-  get(key: string) {
-    return this.#store.get(key);
-  }
+  get = (key: string) => {
+    return this.store.get(key);
+  };
 
-  set(key: string, value: any) {
-    this.#store.set(key, value);
-  }
+  set = (key: string, value: any) => {
+    this.store.set(key, value);
+  };
 
-  delete(key: string) {
-    this.#store.delete(key);
-  }
+  delete = (key: string) => {
+    this.store.delete(key);
+  };
 }
