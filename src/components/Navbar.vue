0bcb02569ae421312839c9434fdaa82cb30ac50d
Joralmo <JoralmoPro@Gmail.com>
2021-04-18 12:14:23 -0500

    add views for app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <span class="mr-2">Lazy Reminder</span>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn to="/" text>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn to="/configuration" text>
+      <v-icon>mdi-settings</v-icon>
+    </v-btn>
+
+    <v-btn to="/about" text>
+      <v-icon>mdi-help</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Navbar",
+
+  data: () => ({}),
+});
+</script>
---
+++ src/views/Configuration.vue
@@ -0,0 +1,84 @@
+<style scoped lang="scss">
+.configuration {
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+}
+.no-margin {
+  display: flex;
+  align-items: center;
+  .col {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
+
+<template>
+  <div class="configuration">
+    <v-container>
+      <!--
+      <v-row class="no-margin">
+        <v-col cols="10">{{ $t("iniWithSO") }}</v-col>
+        <v-col cols="2"><v-switch v-model="startWithOs"></v-switch></v-col>
+      </v-row>
+      -->
+      <v-row class="no-margin">
+        <v-container>
+          <v-row>
+            <v-col cols="12">{{ $t("showBreaksIn") }}:</v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-container fluid>
+                <v-radio-group v-model="showBreaksIn" class="d-flex">
+                  <v-row>
+                    <v-col cols="2"></v-col>
+                    <v-col cols="4"
+                      ><v-radio :label="$t('window')" :value="1"></v-radio
+                    ></v-col>
+                    <v-col cols="5"
+                      ><v-radio :label="$t('fullScreen')" :value="2"></v-radio
+                    ></v-col>
+                  </v-row>
+                </v-radio-group>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-row>
+      <v-row class="no-margin mt-7">
+        <v-container>
+          <v-row>
+            <v-slider
+              v-model="intervalForBreaks"
+              :label="$t('intervalForBreaks')"
+              thumb-label="always"
+              min="5"
+              max="60"
+            ></v-slider>
+          </v-row>
+        </v-container>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    // startWithOs: false,
+    showBreaksIn: null,
+    intervalForBreaks: null,
+  }),
+  mounted: function () {
+    const {
+      electronTools: { showBreaksIn, intervalForBreaks },
+    } = window;
+    this.showBreaksIn = showBreaksIn;
+    this.intervalForBreaks = intervalForBreaks;
+  },
+});
+</script>
