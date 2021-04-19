0a0bc63571ce327288cf49ac3a2b2eadefa324fc
Joralmo <JoralmoPro@Gmail.com>
2021-04-18 22:50:00 -0500

    improve store :fire:

--- src/electron/store.ts
+++ src/electron/store.ts
@@ -1,4 +1,5 @@
 import Store, { Schema } from "electron-store";
+import { Reminder } from "./reminder";
 
 const schema: Schema<Record<string, unknown>> = {
   showBreaksIn: {
@@ -13,12 +14,39 @@ const schema: Schema<Record<string, unknown>> = {
     minimum: 5,
     default: 15,
   },
+  breakDuration: {
+    type: "number",
+    maximum: 60,
+    minimum: 5,
+    default: 15,
+  },
+  showOnAllMonitors: {
+    type: "boolean",
+    default: false,
+  },
+  actualDate: {
+    type: "number",
+    default: 0,
+  },
+  nextDate: {
+    type: "number",
+    default: 0,
+  },
 };
 
 export class LocalStorage {
+  private static instance: LocalStorage;
   private store: Store;
   constructor() {
-    this.store = new Store({ schema });
+    this.store = new Store({ schema, watch: true });
+  }
+
+  public static getInstance(): LocalStorage {
+    if (!LocalStorage.instance) {
+      LocalStorage.instance = new LocalStorage();
+    }
+
+    return LocalStorage.instance;
   }
 
   get = (key: string) => {
@@ -31,5 +59,11 @@ export class LocalStorage {
 
   delete = (key: string) => {
     this.store.delete(key);
+  };
+
+  onChange = (key: string, callback: Function): Function => {
+    return this.store.onDidChange(key, (newValue, oldValue) => {
+      callback(newValue, oldValue);
+    });
   };
 }
