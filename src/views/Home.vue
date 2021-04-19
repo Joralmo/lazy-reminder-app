bf7401d26d41b58541423a75026fba8d2640ef2b
Joralmo <JoralmoPro@Gmail.com>
2021-04-18 22:51:03 -0500

    show time for new break :fire:

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
           <h3>{{ $t("nextBreak") }}</h3>
         </v-row>
         <v-row justify="center">
-          <h2>14 minutos 2 segundos</h2>
+          <h2>{{ date }}</h2>
         </v-row>
       </v-container>
     </v-row>
@@ -17,9 +17,32 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { LocalStorage } from "@/electron/store";
+import moment from "moment";
+import { defaultLocale } from "@/i18n";
+const { Store }: { Store: LocalStorage } = window.electronTools;
 
 export default Vue.extend({
   name: "Home",
   components: {},
+  data: () => ({
+    interval: 0,
+    date: "",
+  }),
+  mounted: function () {
+    moment.locale(defaultLocale);
+    this.getDate();
+  },
+  methods: {
+    getDate: function () {
+      this.interval = window.setInterval(() => {
+        const nextDate = Store.get("nextDate") as number;
+        this.date = moment(nextDate).fromNow();
+      }, 1000);
+    },
+  },
+  destroyed: function () {
+    clearInterval(this.interval);
+  },
 });
 </script>
